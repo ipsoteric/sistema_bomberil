d6fb454075111826774f37bbc622dcfc2f8783a7
Pelaoconbeatle <juan.castillo18@utlook.com>
2025-11-11 14:27:22 -0300

    fix: Corregir la eliminación de la lógica de permisos de módulo

--- apps/gestion_usuarios/apps.py
+++ apps/gestion_usuarios/apps.py
@@ -6,14 +6,14 @@ class UsuariosConfig(AppConfig):
     name = 'apps.gestion_usuarios'
     verbose_name = "Gestión de Usuarios y Permisos"
 
-    def ready(self):
-        import apps.gestion_usuarios.signals
-        from django.db.models.signals import post_migrate
-        from apps.common.utils import crear_permiso_de_acceso_al_modulo
-
-        # 2. Conecta la señal a esa función, pasándole la configuración
-        #    de esta app específica (self).
-        #post_migrate.connect(
-        #    lambda sender, **kwargs: crear_permiso_de_acceso_al_modulo(self),
-        #    sender=self
-        #)
+    #def ready(self):
+    #    import apps.gestion_usuarios.signals
+    #    from django.db.models.signals import post_migrate
+    #    from apps.common.utils import crear_permiso_de_acceso_al_modulo
+    #
+    #    # 2. Conecta la señal a esa función, pasándole la configuración
+    #    #    de esta app específica (self).
+    #    post_migrate.connect(
+    #        lambda sender, **kwargs: crear_permiso_de_acceso_al_modulo(self),
+    #        sender=self
+    #    )
--- apps/gestion_voluntarios/apps.py
+++ apps/gestion_voluntarios/apps.py
@@ -6,13 +6,13 @@ class GestionVoluntariosConfig(AppConfig):
     name = 'apps.gestion_voluntarios'
     verbose_name = 'Gestión de Voluntarios'
 
-    def ready(self):
-        from django.db.models.signals import post_migrate
-        from apps.common.utils import crear_permiso_de_acceso_al_modulo
-
-        # 2. Conecta la señal a esa función, pasándole la configuración
-        #    de esta app específica (self).
-        #post_migrate.connect(
-        #    lambda sender, **kwargs: crear_permiso_de_acceso_al_modulo(self),
-        #    sender=self
-        #)
+    #def ready(self):
+    #    from django.db.models.signals import post_migrate
+    #    from apps.common.utils import crear_permiso_de_acceso_al_modulo
+    #
+    #    # 2. Conecta la señal a esa función, pasándole la configuración
+    #    #    de esta app específica (self).
+    #    post_migrate.connect(
+    #        lambda sender, **kwargs: crear_permiso_de_acceso_al_modulo(self),
+    #        sender=self
+    #    )
