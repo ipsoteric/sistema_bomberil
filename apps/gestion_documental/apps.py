5c8a8127266cac8811a00ce1a88c58b2345df11f
EsotericHog <juan.castillo18@outlook.com>
2025-11-23 18:04:22 -0300

    fix(documental): Definir verbose_name para la app de gestión documental

--- apps/gestion_documental/apps.py
+++ apps/gestion_documental/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GestionDocumentalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.gestion_documental'
+    verbose_name = 'Gestión Documental'
